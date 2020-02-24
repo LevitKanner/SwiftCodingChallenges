228eb5a9aad7b7b03b2c0a3ad5b27dbaf85b1383
Levit Osei Kanner <lkanner21@gmail.com>
2020-02-24 16:46:21 +0000

    Write a function that checks whether two strings are of the same length , do not have more than 3 letters in difference and all common letter are in the same position

--- Contents.swift
+++ Contents.swift
@@ -268,3 +268,44 @@ func vowelsAndConsonants(in input: String) -> (Int , Int) {
 }
 vowelsAndConsonants(in: "Swift Coding Challenges")
 vowelsAndConsonants(in: "Mississippi")
+
+
+/*
+ Write a function that accepts two strings, and returns true if they are identical in length but
+ have no more than three different letters, taking case and string order into account.
+ 
+ Sample input and output
+ • The strings “Clamp” and “Cramp” would return true, because there is one letter difference.
+ • The strings “Clamp” and “Crams” would return true, because there are two letter
+ differences.
+ • The strings “Clamp” and “Grams” would return true, because there are three letter
+ differences.
+ • The strings “Clamp” and “Grans” would return false, because there are four letter
+ differences.
+ • The strings “Clamp” and “Clam” would return false, because they are different lengths.
+ • The strings “clamp” and “maple” should return false. Although they differ by only one
+ letter, the letters that match are in different positions.
+
+ */
+
+func threeLetters(first: String , second: String) -> Bool {
+    guard first.count == second.count else {
+        return false
+    }
+    var difference = 0
+    
+    for (index , letter) in first.enumerated() {
+        if letter != Array(second)[index]{
+            difference += 1
+        }
+    }
+    
+    return difference < 4
+}
+threeLetters(first: "Clamp", second: "Cramp")
+threeLetters(first: "Clamp", second: "Crams")
+threeLetters(first: "Clamp", second: "Grams")
+threeLetters(first: "Clamp", second: "Grans")
+threeLetters(first: "Clamp", second: "Clam")
+threeLetters(first: "clamp", second: "maple")
+
