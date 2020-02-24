05e0151523aa1685b3d72dba9613973a29d565c3
Levit Osei Kanner <lkanner21@gmail.com>
2020-02-24 09:41:00 +0000

    Write a function that tells if a string can be rotated into another

--- Contents.swift
+++ Contents.swift
@@ -195,3 +195,31 @@ func condense(input: String) -> String {
 condense(input: "a   b   c")
 condenseWhiteSpace(in: "    a")
 condense(input: "abc")
+
+
+
+/*
+ Write a function that accepts two strings, and returns true if one string is rotation of the other,
+ taking letter case into account.
+ Tip: A string rotation is when you take a string, remove some letters from its end, then append them to the front. For example, “swift” rotated by two characters would be “ftswi”.
+ 
+ Sample input and output
+ • The string “abcde” and “eabcd” should return true.
+ • The string “abcde” and “cdeab” should return true.
+ • The string “abcde” and “abced” should return false; this is not a string rotation.
+ • The string “abc” and “a” should return false; this is not a string rotation.
+ */
+
+func canRotate(_ input: String , into: String) -> Bool {
+    let parent = input + input
+    
+    guard input.count == into.count else {
+        return false
+    }
+    
+    return parent.contains(into)
+}
+canRotate("abcde", into: "eabcd")
+canRotate("abcde", into: "cdeab")
+canRotate("abcde", into: "abced")
+canRotate("abc", into: "a")
