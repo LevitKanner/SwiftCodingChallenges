71d93994ebb4bb6c6540785c25b48c8433e8655b
Levit Osei Kanner <lkanner21@gmail.com>
2020-02-24 10:49:27 +0000

    Write a function that tells a word is a pangram

--- Contents.swift
+++ Contents.swift
@@ -223,3 +223,26 @@ canRotate("abcde", into: "eabcd")
 canRotate("abcde", into: "cdeab")
 canRotate("abcde", into: "abced")
 canRotate("abc", into: "a")
+
+
+
+/*
+ Write a function that returns true if it is given a string that is an English pangram, ignoring
+ letter case.
+ Tip: A pangram is a string that contains every letter of the alphabet at least once.
+ Sample input and output
+ 
+ • The string “The quick brown fox jumps over the lazy dog” should return true.
+ • The string “The quick brown fox jumped over the lazy dog” should return false, because
+ it’s missing the S.
+ */
+
+func isPangram(input: String) -> Bool {
+    let alphabets = "abcdefghijklmnopqrstuvwxyz"
+    let newWord = Set(input.lowercased()).sorted()
+    return alphabets == String(newWord).replacingOccurrences(of: " ", with: "")
+}
+
+isPangram(input: "The quick brown fox jumps over the lazy dog")
+isPangram(input: "The quick brown fox jumped over the lazy dog")
+
