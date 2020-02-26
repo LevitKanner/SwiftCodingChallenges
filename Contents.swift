c9d591481a9c7fc6e2f580d2d137e08f87a4634a
Levit Osei Kanner <lkanner21@gmail.com>
2020-02-26 10:10:21 +0000

    Write a function that accepts positive minimum and maximum integers, and returns a random  number between those two bounds, inclusive

--- Contents.swift
+++ Contents.swift
@@ -438,3 +438,20 @@ func fizzBuzz(){
     }
 }
 fizzBuzz()
+
+
+/*
+ Write a function that accepts positive minimum and maximum integers, and returns a random
+ number between those two bounds, inclusive.
+ 
+ Sample input and output
+ • Given minimum 1 and maximum 5, the return values 1, 2, 3, 4, 5 are valid.
+ • Given minimum 8 and maximum 10, the return values 8, 9, 10 are valid.
+ • Given minimum 12 and maximum 12, the return value 12 is valid.
+ • Given minimum 12 and maximum 18, the return value 7 is invalid.
+ */
+func randomNumber(between: Int , and: Int) -> Int {
+    let array = [between , and].sorted()
+    return (array[0]...array[1]).randomElement()!
+}
+randomNumber(between: -1, and: -4)
