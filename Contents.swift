f402c35b9d7c30c636918e5ca3c62e3d4dd42d08
Levit Osei Kanner <lkanner21@gmail.com>
2020-03-04 07:23:00 +0000

    Challenge 43:  Create a linked list of lowercase English alphabet letters, along with a method that traverses all  nodes and prints their letters on a single line separated by spaces

--- Contents.swift
+++ Contents.swift
@@ -949,3 +949,58 @@ extension Collection where Iterator.Element: Equatable {
 [1, 2, 3].indexFor(element: 1)
 [1, 2, 3].indexFor(element: 3)
 [1, 2, 3].indexFor(element: 5)
+
+
+
+
+
+/*
+ Create a linked list of lowercase English alphabet letters, along with a method that traverses all
+ nodes and prints their letters on a single line separated by spaces.
+ 
+ Sample input and output
+ • The output of your code should be the English alphabet printed to the screen, i.e. “a b c d ... x y z”.
+ */
+
+///Create a node class
+class LinkedListNode<T> {
+    var value: T
+    var next: LinkedListNode?
+    
+    init(value: T) {
+        self.value = value
+    }
+}
+
+//Creates a list class with the method to print nodes
+class LinkedList<T> {
+    var start: LinkedListNode<T>?
+    
+    //Print method
+    func printNodes(){
+        var currentNode = start
+        
+        while let node = currentNode {
+            print(node.value , terminator: " ")
+            currentNode = node.next
+        }
+    }
+}
+
+
+
+let list = LinkedList<Character>()
+var previousNode: LinkedListNode<Character>? = nil
+
+for letter in "abcdefghijklmnopqrstuvwxyz" {
+    let node = LinkedListNode(value: letter)
+    
+    if let predecessor = previousNode {
+        predecessor.next = node
+    }else {
+        list.start = node
+    }
+    previousNode = node
+}
+
+list.printNodes()
